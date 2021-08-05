4923dbe65afe270fa26e473edeb4ea579d1ccf3a
CatKatrin-PC\CatKatrin <Proskunova.K@gmail.com>
2021-08-05 21:41:31 +0300

    UI

---
+++ Stone Age Group1/Assets/Scripts/HeartController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartController : MonoBehaviour
+{
+    [SerializeField] private Image _heart;
+    [Header("Full")]
+    [SerializeField] private Sprite _spriteFull;
+    [SerializeField] private Color _colorFull;
+    [Header("Empty")]
+    [SerializeField] private Sprite _spriteEmpty;
+    [SerializeField] private Color _colorEmpty;
+
+    private void Awake()
+    {
+        if (_heart == null) throw new UnityException("_heart == null");
+    }
+
+    public void SetFull()
+    {
+        _heart.sprite = _spriteFull;
+        _heart.color = _colorFull;
+    }
+
+    public void SetEmpty()
+    {
+        _heart.sprite = _spriteEmpty;
+        _heart.color = _colorEmpty;
+
+    }
+
+
+
+
+
+
+
+
+}
