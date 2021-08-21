51679bbd1b68b586bcdee159f66f203588f9aebf
CatKatrin-PC\CatKatrin <Proskunova.K@gmail.com>
2021-08-21 15:13:54 +0300

    Голод. Прыжки

---
+++ Stone Age Group1/Assets/Scripts/Indicators.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Indicators : MonoBehaviour
+{
+    public Image foodBar;
+    public float foodAmount = 100f;
+    public float secondsToEmptyFood;
+
+    
+    void Start()
+    {
+        foodBar.fillAmount = foodAmount / 100;
+    }
+
+    void Update()
+    {
+        if(foodAmount > 0)
+        {
+            foodAmount -= 100 / secondsToEmptyFood * Time.deltaTime;
+            foodBar.fillAmount = foodAmount / 100;
+        }
+    }
+
+        
+    
+
+}
--- Stone Age Group1/Assets/Scripts/PlayerMove.cs
+++ Stone Age Group1/Assets/Scripts/PlayerMove.cs
@@ -6,60 +6,77 @@ using UnityEngine;
 [RequireComponent(typeof (Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
-    [Header("Plaeye")]
+    [Header("Player")]
     [SerializeField] private float playerSpeed;
     [SerializeField] private float playerJumpForce;
 
+    [SerializeField] private GameObject animObject;
+
+    private SpriteRenderer spriteRenderer;
+
+
     private float currentPlayerSpeed;
     private Rigidbody2D rb;
-    private bool grounCheck;
+    private bool groundCheck;
+
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = animObject.GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         // Enum using
         rb.velocity = new Vector2(currentPlayerSpeed * Time.deltaTime, rb.velocity.y);
+        //rb.AddForce(new Vector2(currentPlayerSpeed, 0f), ForceMode2D.Force);
     }
 
-
-
-    //Flip to Flip (разворот корректный сделать при хотьбе)
     public void RightMove()
     {
         currentPlayerSpeed = playerSpeed;
-        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        spriteRenderer.flipX = false;
+       
     }
     public void LeftMove()
     {
         currentPlayerSpeed = - playerSpeed;
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        spriteRenderer.flipX = true;
+       
     }
-
+  
     public void StopMove()
     {
         currentPlayerSpeed = 0f;
+        //rb.velocity = new Vector2(currentPlayerSpeed, rb.velocity.y);
     }
 
     public void Jump()
     {
-
+        if (groundCheck)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, playerJumpForce);
+            groundCheck = false;
+        }
     }
 
-
-
-
-
-
-
-
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        groundCheck = true;
+    }
 
 
 
 
 
 }
+
+//if (transform.localScale.x < 0)
+//{
+//    transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+//}
+//if (transform.localScale.x > 0)
+//{
+//    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+//}
